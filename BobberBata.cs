710b69a13cf5a49b8a1dfa3c745a04b60053e721
--unset <--unset>
2025-08-12 03:01:43 -0400

    New BobberBata class.

---
+++ BobberBata.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BebberBobbers;
+
+public class BobberBata
+{
+    public string Id = null!;
+    public string? Texture = null;
+    public int SpriteIndex = 0;
+    public string? Condition;
+    public bool FlipWhenFacingRight;
+    public string? FishingLineColor;
+    public string? FishingLineColour;
+}
